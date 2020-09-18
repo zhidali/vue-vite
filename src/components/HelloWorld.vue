42ed84e9e65c28fdfd80735481447834a860748c
zhidl <zhidali@julive.com>
2020-09-18 09:45:56 +0800

    [updata] vue外壳

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <button @click="a++">a is: {{ a }}</button>
+  {{b}}
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+import {ref, reactive, computed} from 'vue';
+import {effect} from '@vue/reactivity'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup(){
+    let a = ref(0);
+    a.value++;
+    let b = computed(() => a.value * 2);
+
+    // effect(() => {
+    //   console.log(a);
+    // })
+    return{a, b}
+  },
+
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
